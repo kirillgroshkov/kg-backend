22b2f6b6fa63fbac29621c00c078d8d76432b59b
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2018-02-24 15:11:32 +0100

    fix

--- src/cnst/paths.cnst.ts
+++ src/cnst/paths.cnst.ts
@@ -1,6 +1,6 @@
 import * as path from 'path'
 
 export const projectDir = path.join(__dirname, '/../..')
-export const rootDir = projectDir + '/src'
+export const rootDir = path.join(__dirname, '/..')
 export const staticDir = rootDir + '/static'
 export const schemaDir = rootDir + '/schema'
