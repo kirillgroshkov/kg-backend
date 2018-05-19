bbe844f4e075e84af252d2cd2d9f6873b35d0c51
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2018-05-19 10:59:58 +0200

    try

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,5 @@
 // Symbol.asyncIterator
 // from: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-3.html
-(Symbol as any).asyncIterator = Symbol.asyncIterator || Symbol.for('Symbol.asyncIterator')
+if (!(Symbol as any).asyncIterator) {
+  (Symbol as any).asyncIterator = Symbol.for('Symbol.asyncIterator')
+}
